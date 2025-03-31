b69a06be279c056c1d0b2ad3be3a9e95f5d1b079
jerebenitez <jeremias.benitez@mi.unc.edu.ar>
2025-03-31 13:49:20 -0300

    pointers, lesson 1

---
+++ 10_pointers/L1/go.mod
@@ -0,0 +1,3 @@
+module boot.dev-go/10_pointers/L1/main
+
+go 1.21.4
---
+++ 10_pointers/L1/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+type Message struct {
+	Recipient string
+	Text      string
+}
+
+func getMessageText(m Message) string {
+	return fmt.Sprintf(`
+To: %v
+Message: %v
+`, m.Recipient, m.Text)
+}
+
