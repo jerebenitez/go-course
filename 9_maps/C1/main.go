75bf4aa16374e02ff20e1909e7e00ff6f9944f1b
jerebenitez <jeremias.benitez@mi.unc.edu.ar>
2025-03-31 07:38:04 -0300

    maps, challenge 1

---
+++ 9_maps/C1/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"strings"
+)
+
+func countDistinctWords(messages []string) int {
+    words := make(map[string]bool)
+
+    for _, msg := range messages {
+        if msg == "" {
+            continue
+        }
+
+        for _, word := range strings.Split(msg, " ") {
+            words[strings.ToLower(word)] = true
+        } 
+    }
+
+    return len(words)
+}
