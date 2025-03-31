04715b4cfd9eb0ecff09634e72405d9d325a6dc8
jerebenitez <jeremias.benitez@mi.unc.edu.ar>
2025-03-31 13:53:55 -0300

    pointers, lesson 2

---
+++ 10_pointers/L2/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"strings"
+)
+
+func removeProfanity(message *string) {
+    *message = strings.ReplaceAll(*message, "fubb", "****")
+    *message = strings.ReplaceAll(*message, "shiz", "****")
+    *message = strings.ReplaceAll(*message, "witch", "*****")
+}
+
