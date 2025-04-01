4f771aa101d27f99336a54242fbecba82985962a
jerebenitez <jeremias.benitez@mi.unc.edu.ar>
2025-03-31 21:12:56 -0300

    enums, lesson 1

---
+++ 15_enums/L1/main.go
@@ -0,0 +1,17 @@
+package L1
+
+import "fmt"
+
+func (a *analytics) handleEmailBounce(em email) error {
+
+    if err := em.recipient.updateStatus(em.status); err != nil {
+        return fmt.Errorf("error updating user status: %w", err)
+    }
+
+    if err := a.track(em.status); err != nil {
+        return fmt.Errorf("error tracking user bounce: %w", err)
+    }
+	
+    return nil
+}
+
